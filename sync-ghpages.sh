19d67ff63abf8a4ad37d9616663de01da7645f71
theborakompanioni <theborakompanioni+github@gmail.com>
2015-05-04 22:28:45 +0200

    update sync-ghpages.sh
    change temporary directory

--- sync-ghpages.sh
+++ sync-ghpages.sh
@@ -5,13 +5,13 @@ echo "Generating app .."
 gulp build
 if [[ ${PIPESTATUS[0]} -eq 0 ]]; then
   echo "Copying from dist/app/ to temporary directory .."
-  mkdir tmp-gh-pages
-  cp -R dist/app tmp-gh-pages/app
+  mkdir ./.tmp/tmp-gh-pages
+  cp -R dist/app ./.tmp/tmp-gh-pages/app
   echo "Switching to branch gh-pages .."
   git checkout gh-pages
   if [[ ${PIPESTATUS[0]} -eq 0 ]]; then
     echo "Copying from temporary directory to ./ .."
-    cp -R tmp-gh-pages/app/* ./
+    cp -R ./.tmp/tmp-gh-pages/app/* ./
     read -p "Enter a commit message: " commitMessage
     git status
     git add -A .
@@ -19,7 +19,7 @@ if [[ ${PIPESTATUS[0]} -eq 0 ]]; then
     echo "Pushing to remote branch gh-pages .."
     git push origin gh-pages
     echo "Removing directory temporary directory .."
-    rm -rf tmp-gh-pages
+    rm -rf ./.tmp/tmp-gh-pages
     echo "Switching back to branch master .."
     git checkout master
   fi
